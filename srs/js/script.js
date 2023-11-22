e0914f7170f981930ab22948ce586c89750bd77d
Artur Kutsev <kutsev11b@gmail.com>
2023-11-22 16:18:59 +0300

    burger + media

---
+++ srs/js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded",function(){
+    document.getElementById("burger__menu-btn").addEventListener("click",function(){
+        document.querySelector(".burger__menu__items").classList.toggle("burger__menu__items-active")
+    }
+    )
+}
+)
+
